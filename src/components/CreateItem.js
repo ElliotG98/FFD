b33913caa492307542148b77dd56d3dc9db24e64
Elliot Glaze <elliotglaze98@gmail.com>
2021-05-21 12:57:51 +0100

    Create todo list item cleanup

--- src/components/CreateItem.js
+++ src/components/CreateItem.js
@@ -1,56 +1,90 @@
-import React, { Component} from 'react';
-import {Form, FormGroup, FormControl, FormLabel, Modal, Button} from 'react-bootstrap';
+import React, { Component, setState} from 'react';
 import Amplify, {API} from 'aws-amplify';
 
 
 export default class CreateItem extends Component{
+    constructor(props) {
+		super(props);
 
-    constructor(props){
-        super(props);
-        this.state = {
-            Items: {
-                id: '',
-                completed: '',
-                task: ''
-            }
-        };
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
+		this.state = {
+			id: '',
+			completed: false,
+			task: ''
+		}
+	}
+
+	handleIdChange = event => {
+		this.setState({
+			id: event.target.value
+		});
+	}
+
+	handleCompletedChange = event => {
+		this.setState({
+			completed: event.target.value
+		});
+	}
+
+	handleTaskChange = event => {
+		this.setState({
+			task: event.target.value
+		});
+	}
+
+	handleSubmit = event => {
+		alert(`${this.state.id} ${this.state.completed} ${this.state.task}`);
+		event.preventDefault();
+
+        this.postToDoList();
+	}
+
+    postToDoList(){
+        return fetch("https://5tkwxph66b.execute-api.us-east-2.amazonaws.com/items", {
+            method: 'POST',
+            headers: {
+                "Content-type": "application/json"
+
+            },
+            body: JSON.stringify({id: this.state.id, completed: this.state.completed, task: this.state.task})
+        })
+        .then((res) => res.json())
+        .then(data => {
+            console.log(data);
+        })
+        .catch((err) => {
+            console.log(err);
+        })
     }
 
-    async handleChange(event){
-        console.log(event.target.value);
-        const target = event.target;
-        const value = target.type === 'checkbox' ? target.checked : target.value;
-        const name = target.name;
-
-        await this.setState({load: true});
-        this.setState({[name]: value});
-        await this.setState({load: false});
-    }
-
-    handleSubmit(event){
-        alert('A to do was submitted: ' + this.state.Items.task + this.state.Items.completed + this.state.Items.id);
-        event.preventDefault();
-    }
-
-    render() {
-        return (
-            <form onSubmit={this.handleSubmit}>
-                <label>
-                    ID
-                    <input name="id" type="number" defaultValue={this.state.Items.id} onChange={this.handleChange}/>
-                </label>
-                <label>
-                    completed
-                    <input name="completed" type="checkbox" defaultChecked={this.state.Items.completed} onChange={this.handleChange}/>
-                </label>
-                <label>
-                    Task
-                    <input type="text" defaultValue={this.state.Items.task} onChange={this.handleChange}/>
-                </label>
-                <input type="submit" value="Submit"/>
-            </form>
-        );
-    }
+	render() {
+		const { id, completed, task } = this.state
+		return (
+			<form onSubmit={this.handleSubmit}>
+				<div>
+					<label>id </label>
+					<input
+						type="text"
+						value={id}
+						onChange={this.handleIdChange}
+					/>
+				</div>
+				<div>
+					<label>completed</label>
+					<select value={completed} onChange={this.handleCompletedChange}>
+						<option value={true}>Completed</option>
+						<option value={false}>Unfinished</option>
+					</select>
+				</div>
+                <div>
+					<label>task </label>
+					<input
+						type="text"
+						value={task}
+						onChange={this.handleTaskChange}
+					/>
+				</div>
+				<button type="submit">Submit</button>
+			</form>
+		)
+	}
 }
